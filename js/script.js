8910c4bed7a37ff295c2ff2c953069fefa1cba37
munyekenye <munyekenyelilian@gmail.com>
2019-12-08 09:20:43 +0300

    Added script for What we do section

--- js/script.js
+++ js/script.js
@@ -1,12 +1,46 @@
 $(document).ready(function () {
-    $("#wedo").hide();
-    $("#wedo1").hide();
-    $(".toggle1 ,.toggle2").click(function () {
-        $("#wedo1").toggle();
-        $(".toggle1").toggle();
-    });
-    $(".toggle3 , .toggle4").click(function () {
-        $("#wedo1").toggle();
-        $(".toggle3").toggle();
-    });
-});
+    $(document).ready(function(){
+
+        $("#dotext").hide(); 
+       
+        $("#dotext2").hide();
+       
+        $("#dotext3").hide(); 
+       
+       $(".imagetoggle0 , .imagetoggle1").click(function(){
+       
+       $("#dotext").toggle();
+       
+       $(".imagetoggle0").toggle();
+       
+       });
+       
+       $(".imagetoggle2 , .imagetoggle3").click(function(){
+       
+       $("#dotext2").toggle();
+       
+       $(".imagetoggle2").toggle();
+       
+       });
+       
+       $(".imagetoggle4 , .imagetoggle5").click(function(){
+       
+       $("#dotext3").toggle();
+       
+       $(".imagetoggle4").toggle();    
+       
+       });
+       
+       });
+       
+       
+       
+       $(document).ready(function(){
+       
+       $("form").submit(function(){
+       
+       alert("Thanks  for being our client ,your message has been received");
+       
+       });
+       
+       
